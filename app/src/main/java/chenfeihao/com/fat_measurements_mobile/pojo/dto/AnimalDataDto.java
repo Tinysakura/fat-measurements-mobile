6eda4b63b76802ea3e61fff10fc72bbfb67fa5b4
chenfeihao <chenfeihao@corp.netease.com>
2019-03-17 14:04:23 +0800

    @dev 引入lombok插件

---
+++ app/src/main/java/chenfeihao/com/fat_measurements_mobile/pojo/dto/AnimalDataDto.java
@@ -0,0 +1,8 @@
+package chenfeihao.com.fat_measurements_mobile.pojo.dto;
+
+/**
+ * @Author: chenfeihao@corp.netease.com
+ * @Date: 2019/3/17
+ */
+public class AnimalDataDto {
+}
---
+++ app/src/main/java/chenfeihao/com/fat_measurements_mobile/pojo/info.java
@@ -0,0 +1,8 @@
+package chenfeihao.com.fat_measurements_mobile.pojo;
+
+/**
+ * @Author: chenfeihao@corp.netease.com
+ * @Date: 2019/3/17
+ */
+public class info {
+}
