93b123518cda3fc326e48500d50ae1e215c6f3b4
chenfeihao <chenfeihao@corp.netease.com>
2019-03-17 16:55:36 +0800

    @dev 注册界面ui开发

---
+++ app/src/main/java/chenfeihao/com/fat_measurements_mobile/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package chenfeihao.com.fat_measurements_mobile.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import chenfeihao.com.fat_measurements_mobile.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
