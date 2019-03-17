96576e55486751bc0569b4d633bd3d7b46bc1bba
chenfeihao <chenfeihao@corp.netease.com>
2019-03-17 14:37:10 +0800

    @dev 头像上传retrofit接口定义

---
+++ app/src/main/java/chenfeihao/com/fat_measurements_mobile/http/constant/CommonConstant.java
@@ -0,0 +1,12 @@
+package chenfeihao.com.fat_measurements_mobile.http.constant;
+
+/**
+ * @Author: chenfeihao@corp.netease.com
+ * @Date: 2019/3/17
+ */
+public class CommonConstant {
+
+    public class ServiceHostConstant {
+        private static final String host = "http://10.0.0.2:8080/";
+    }
+}
--- app/src/main/java/chenfeihao/com/fat_measurements_mobile/http/retrofit/UserHttpService.java
+++ app/src/main/java/chenfeihao/com/fat_measurements_mobile/http/retrofit/UserHttpService.java
@@ -2,9 +2,12 @@ package chenfeihao.com.fat_measurements_mobile.http.retrofit;
 
 import chenfeihao.com.fat_measurements_mobile.http.common.ResponseView;
 import chenfeihao.com.fat_measurements_mobile.pojo.dto.UserDto;
+import okhttp3.MultipartBody;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -18,4 +21,8 @@ public interface UserHttpService {
 
     @GET("/a/login")
     Observable<ResponseView> login(@Query("userName") String userName, @Query("userPassword") String userPassword);
+
+    @Multipart
+    @POST("/a/set/headportrait")
+    Observable<ResponseView>  uploadHeadPortrait(@Part MultipartBody.Part headPortrait);
 }
