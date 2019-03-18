150f5d1945a0c7ca021b7c02bc055d12dc3a0637
chenfeihao <chenfeihao@corp.netease.com>
2019-03-18 16:39:17 +0800

    @dev 当app从后台回到前台时若用户未登陆则跳转到LoginActivity

--- app/src/main/java/chenfeihao/com/fat_measurements_mobile/activity/MainActivity.java
+++ app/src/main/java/chenfeihao/com/fat_measurements_mobile/activity/MainActivity.java
@@ -1,9 +1,11 @@
 package chenfeihao.com.fat_measurements_mobile.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import chenfeihao.com.fat_measurements_mobile.R;
+import chenfeihao.com.fat_measurements_mobile.app.App;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        jump2Login();
+    }
+
+    private void jump2Login() {
+        if (getApp().getMobileUser() == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+
+    private App getApp() {
+        return (App)getApplicationContext();
     }
 }
