e33c33e99bb0033c79680d9c4179e6b5e8b1c9bb
chenfeihao <chenfeihao@corp.netease.com>
2019-03-18 14:22:49 +0800

    @dev app启动用户信息初始化

--- app/src/main/java/chenfeihao/com/fat_measurements_mobile/app/App.java
+++ app/src/main/java/chenfeihao/com/fat_measurements_mobile/app/App.java
@@ -1,8 +1,15 @@
 package chenfeihao.com.fat_measurements_mobile.app;
 
 import android.app.Application;
+import android.content.Intent;
+import android.content.SharedPreferences;
 
+import com.alibaba.fastjson.JSON;
+
+import chenfeihao.com.fat_measurements_mobile.activity.LoginActivity;
 import chenfeihao.com.fat_measurements_mobile.http.constant.CommonConstant;
+import chenfeihao.com.fat_measurements_mobile.pojo.bo.MobileUser;
+import chenfeihao.com.fat_measurements_mobile.util.StringUtil;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 
@@ -16,6 +23,8 @@ public class App extends Application {
 
     private Retrofit retrofit;
 
+    private MobileUser mobileUser;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -24,6 +33,25 @@ public class App extends Application {
                 .baseUrl(CommonConstant.ServiceHostConstant.host)
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
+
+        initUserInfo();
+    }
+
+    private void initUserInfo() {
+        SharedPreferences sharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE);
+        String mobileUserJsonStr = sharedPreferences.getString("mobile_user", null);
+        MobileUser mobileUser = JSON.parseObject(mobileUserJsonStr, MobileUser.class);
+
+        /**
+         * 如果没有用户信息或用户信息过期则跳转到LoginActivity
+         */
+        if (StringUtil.isEmpty(mobileUserJsonStr) || mobileUser.getValidTime().compareTo(System.currentTimeMillis()) < 0) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        } else {// 否则将用户信息塞入全局变量中
+            this.mobileUser = mobileUser;
+        }
+
     }
 
     public String getUserId() {
@@ -36,5 +64,9 @@ public class App extends Application {
 
     public Retrofit getRetrofit() {
         return retrofit;
+    }
+
+    public MobileUser getMobileUser() {
+        return mobileUser;
     }
 }
