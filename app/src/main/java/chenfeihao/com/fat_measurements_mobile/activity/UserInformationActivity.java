4e7174c18488a1c9b98b1d36322e90d57e0d4269
chenfeihao <chenfeihao@corp.netease.com>
2019-03-20 15:30:55 +0800

    @dev 用户信息编辑界面ui开发

--- app/src/main/java/chenfeihao/com/fat_measurements_mobile/activity/UserInformationActivity.java
+++ app/src/main/java/chenfeihao/com/fat_measurements_mobile/activity/UserInformationActivity.java
@@ -2,14 +2,27 @@ package chenfeihao.com.fat_measurements_mobile.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import chenfeihao.com.fat_measurements_mobile.R;
+import chenfeihao.com.fat_measurements_mobile.custom.layout.TitleLayout;
 
 public class UserInformationActivity extends AppCompatActivity {
+    /**
+     * UI
+     */
+    private TitleLayout cutomeTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_information);
+
+        initUI();
+    }
+
+    private void initUI() {
+        cutomeTitle = findViewById(R.id.user_custom_title);
+        cutomeTitle.setTitle("编辑资料");
     }
 }
