04f11dab1ce28e71f49e1afe54669c306f8cc832
chenfeihao <chenfeihao@corp.netease.com>
2019-03-20 14:34:47 +0800

    @dev navigationview绑定点击事件

--- app/src/main/java/chenfeihao/com/fat_measurements_mobile/activity/MainActivity.java
+++ app/src/main/java/chenfeihao/com/fat_measurements_mobile/activity/MainActivity.java
@@ -1,18 +1,53 @@
 package chenfeihao.com.fat_measurements_mobile.activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import chenfeihao.com.fat_measurements_mobile.R;
 import chenfeihao.com.fat_measurements_mobile.app.App;
+import chenfeihao.com.fat_measurements_mobile.constant.UserInformationConstant;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * UI
+     */
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initUI();
+    }
+
+    private void initUI(){
+        navigationView = findViewById(R.id.nav_view);
+        // 设置menu中的data item被默认选中
+        navigationView.setCheckedItem(R.id.nav_data);
+
+        // 给menu中的item设置点击事件
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId()) {
+                case R.id.nav_user:
+                    Intent intent = new Intent(MainActivity.this, UserInformationActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nav_data:
+                    break;
+                case R.id.nav_account:
+                    break;
+                case R.id.nav_setting:
+                    break;
+            }
+
+            return true;
+        });
     }
 
     @Override
---
+++ app/src/main/java/chenfeihao/com/fat_measurements_mobile/activity/UserInformationActivity.java
@@ -0,0 +1,15 @@
+package chenfeihao.com.fat_measurements_mobile.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import chenfeihao.com.fat_measurements_mobile.R;
+
+public class UserInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_information);
+    }
+}
