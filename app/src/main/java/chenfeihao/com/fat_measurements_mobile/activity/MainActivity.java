cf9dcbd68f0b4490cba3bcf2ef4107711f0255c4
chenfeihao <chenfeihao@corp.netease.com>
2019-03-22 16:57:23 +0800

    @fix

--- app/src/main/java/chenfeihao/com/fat_measurements_mobile/activity/MainActivity.java
+++ app/src/main/java/chenfeihao/com/fat_measurements_mobile/activity/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.SearchView;
+import android.support.v7.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
